123a87e5b6ab1e42d01d199850c144f9f34a7890
NusratJahanNisha <nishanusrat646@gmail.com>
2020-11-06 10:09:52 +0600

    done

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -2,9 +2,80 @@ import React from 'react';
 
 const Blogs = () => {
     return (
-        <div style={{textAlign:"center",color:"white", display:"cover",height:"100vh", fontSize:"120px", marginTop:"150px"}} >
-            <p>Coming soon</p>
-        </div>
+        <div style={{}} >
+            <div class="card-deck" style={{margin:"30px"}}>
+                <div class="card box" >
+                    <img class="img-fluid" src="https://i.ibb.co/5rBqf7P/Javascript-History-Image.jpg" class="card-img-top" alt="..." />
+                    <div class="card-body" style={{ color: "white", background: "black" }}>
+                        <h5 style={{ textAlign: "center" }} class="card-title">Solution of 10 real-life problems with the help of javascript.</h5>
+                        <a target="_blank" href="https://nusrat-jahan-nisha.medium.com/solution-of-10-real-life-problems-with-the-help-of-javascript-77c34a3935d8"><button style={{ width: "305px", marginBottom: "7px" }} type="button" class="btn btn-outline-info ml-2">
+                            <b>Read</b>
+                        </button></a> <br />
+
+                    </div>
+                </div>
+                <div class="card box">
+                    <img class="img-fluid" src="https://i.ibb.co/nfz1bxq/react.webp" class="card-img-top" alt="..." />
+                    <div class="card-body" style={{ color: "white", background: "black" }}>
+                        <h5 style={{ textAlign: "center" }} class="card-title">10 things you need to know about React</h5>
+                        <a target="_blank" href="https://nusrat-jahan-nisha.medium.com/10-things-you-need-to-know-about-react-438e7d2961db"><button style={{ width: "305px", marginBottom: "7px" }} type="button" class="btn btn-outline-info ml-2">
+                            <b>Read</b>
+                        </button></a> <br />
+
+                    </div>
+                </div>
+                <div class="card box" >
+                    <img class="img-fluid"  style={{height:"220px"}} src="https://i.ibb.co/xqQrWMy/photo-1515879218367-8466d910aaa4.jpg" class="card-img-top" alt="..." />
+                    <div class="card-body" style={{ color: "white", background: "black" }}>
+                        <h5 style={{ textAlign: "center" }} class="card-title">Secrets of coding style and good commenting</h5>
+                        <a target="_blank" href="https://nusrat-jahan-nisha.medium.com/secrets-of-coding-style-and-good-commenting-381ceb7dfe0e"><button style={{ width: "305px", marginBottom: "7px" }} type="button" class="btn btn-outline-info ml-2">
+                            <b>Read</b>
+                        </button></a> <br />
+                    </div>
+                </div>
+            </div>
+
+
+
+            <div class="card-deck" style={{margin:"30px"}}>
+                <div class="card box">
+                    <img class="img-fluid" style={{height:"220px"}} src="https://i.ibb.co/ysbckyP/Practicalwaysto-Write-Better-JS.png" class="card-img-top" alt="..." />
+                    <div class="card-body" style={{ color: "white", background: "black" }}>
+                        <h5 style={{ textAlign: "center" }} class="card-title">10 interesting things I learned about javascript.</h5>
+                        <a target="_blank" href="https://nusrat-jahan-nisha.medium.com/first-story-a955317ab224"><button style={{ width: "305px", marginBottom: "7px" }} type="button" class="btn btn-outline-info ml-2">
+                            <b>Read</b>
+                        </button></a> <br />
+
+                    </div>
+                </div>
+                <div class="card box">
+                    <img class="img-fluid" style={{height:"220px"}} src="https://i.ibb.co/ctwQcy9/39039130-s.jpg" class="card-img-top" alt="..." />
+                    <div class="card-body" style={{ color: "white" , background: "black"}}>
+                        <h5 style={{ textAlign: "center" }} class="card-title">Let’s handle javascript errors</h5>
+                        <a target="_blank" href="https://nusrat-jahan-nisha.medium.com/lets-handle-javascript-errors-c1107ca31eeb"><button style={{ width: "305px", marginBottom: "7px" }} type="button" class="btn btn-outline-info ml-2">
+                            <b>Read</b>
+                        </button></a> <br />
+
+                    </div>
+                </div>
+                <div class="card" style={{background:"black"}}>
+                    {/* <img class="img-fluid" src="https://i.ibb.co/ctwQcy9/39039130-s.jpg" class="card-img-top" alt="..." />
+                    <div class="card-body" style={{ color: "white" , background: "black"}}>
+                        <h5 style={{ textAlign: "center" }} class="card-title">Let’s handle javascript errors</h5>
+                        <a target="_blank" href="https://nusrat-jahan-nisha.medium.com/lets-handle-javascript-errors-c1107ca31eeb"><button style={{ width: "305px", marginBottom: "7px" }} type="button" class="btn btn-outline-info ml-2">
+                            <b>Read</b>
+                        </button></a> <br /> */}
+
+                    {/* </div> */}
+                </div>
+
+            </div>
+
+
+
+
+
+        </div >
     );
 };
 
